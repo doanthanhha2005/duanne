3ad76939dfe41b7d4abe8297d4db7285b7e74932
doanthanhha2005 <hadtph52997@gmail.com>
2024-11-21 23:04:02 +0700

    Add files via upload

---
+++ duan1/config/config.php
@@ -0,0 +1,8 @@
+<?php
+const DB_HOST = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
+const DB_CHARSET = 'utf8';
+const DB_NAME = 'duan1';
+const BASE_URL = 'http://127.0.0.1';
+?>
---
+++ duan1/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'Models/connectDB.php';
+
+const connectDB = new ConnectDB();
+
+?>
